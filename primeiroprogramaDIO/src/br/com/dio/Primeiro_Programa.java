407c0aff7fa2c9cd166adc0d3d61a22e965cf9f8
Eurides Bonecher Filho <euridesb@hotmail.com>
2022-10-20 14:54:26 -0300

    att Dio

---
+++ primeiroprogramaDIO/src/br/com/dio/Primeiro_Programa.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class Primeiro_Programa {
+
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+        /* int a = 6;
+        int b = 5;
+        System.out.println("Hello bom dia " + (a + b) );*/
+    }
+}
